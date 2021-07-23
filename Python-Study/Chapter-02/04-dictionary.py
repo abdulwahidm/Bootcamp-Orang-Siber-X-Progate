cfe8e9fd64ec9f82c137d0eca8272b04f0acbb25
Abdul Wahid Muhaemin <69133286+abdulwahidm@users.noreply.github.com>
2021-07-24 00:31:20 +0700

    Create 04-dictionary.py
    definisi dictionary

---
+++ Python-Study/Chapter-02/04-dictionary.py
@@ -0,0 +1,6 @@
+'''
+Seperti list, dictionary python digunakan untuk mengelola kelompok data. 
+Perbedaannya adalah dictionary menggunakan kunci (key) daripada nomor index. 
+Pada dictionary, sebuah kunci dipasangkan dengan sebuah nilai (value) untuk membentuk satu element. 
+Hal ini juga dikenal sebagai pasangan kunci-nilai (key-value).
+'''
