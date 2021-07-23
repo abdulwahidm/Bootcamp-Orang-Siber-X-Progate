1527bd3143f2509ac3a1ba4356bde48b2c013b77
abdulwahidm <wamuhabdul@gmail.com>
2021-07-23 23:47:54 +0700

    pengenalan list

---
+++ Python-Study/Chapter-02/01-menyusun-data-dengan-list.py
@@ -0,0 +1,12 @@
+'''
+Mari belajar bagaimana mengelola sekelompok data dengan satu variable. 
+Ketika ada daftar nama-nama makanan, sebagai contoh, tidak efisien untuk menamainya dengan variable terpisah, 
+seperti food1, food2, food3. Akan lebih baik untuk mempunyai variable foods untuk mengelola keseluruhan daftar tersebut.
+
+Seperti integer dan string, kita dapat menentukan list ke dalam satu variable. 
+Sesuai norma penamaan yang berlaku, nama variable bersifat plural, 
+seperti foods, karena variable akan mengandung banyak element.
+'''
+
+foods = ['gulai', 'sushi', 'mie']
+print(foods)
