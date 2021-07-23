35aef5324da4064719fd8106c6da17573e4fc63d
abdulwahidm <wamuhabdul@gmail.com>
2021-07-24 00:12:34 +0700

    menambahkan elemet pada list

--- Python-Study/Chapter-02/02-mengupdate-list.py
+++ Python-Study/Chapter-02/02-mengupdate-list.py
@@ -1,5 +1,6 @@
 #Memperbarui list dapat dilakukan dengan menulis list[index] = nilai.
 foods = ['pasta', 'gulai', 'sushi']
+
 foods[2] = 'mie'#memperbarui element index 2
 print(foods) #['pasta', 'gulai', 'mie']
 
