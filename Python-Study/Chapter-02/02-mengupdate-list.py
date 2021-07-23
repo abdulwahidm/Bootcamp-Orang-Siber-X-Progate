61ac43bf654a6f6acfb1468f88ce35ee7d04f3a6
abdulwahidm <wamuhabdul@gmail.com>
2021-07-24 00:07:12 +0700

    contoh pertama

---
+++ Python-Study/Chapter-02/02-mengupdate-list.py
@@ -0,0 +1,4 @@
+#Memperbarui list dapat dilakukan dengan menulis list[index] = nilai.
+foods = ['pasta', 'gulai', 'sushi']
+foods[2] = 'mie'#memperbarui element index 2
+print(foods) #['pasta', 'gulai', 'mie']
