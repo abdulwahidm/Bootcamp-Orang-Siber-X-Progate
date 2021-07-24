ddf02c1a636dd4108a6d33eb899d536bcef61d2b
abdulwahidm <wamuhabdul@gmail.com>
2021-07-25 05:37:44 +0700

    function

---
+++ Python-Study/Chapter-03/01-function.py
@@ -0,0 +1,14 @@
+
+# Pertama-tama, mari kita lihat bagaimana function bekerja dalam Python. 
+# Function adalah bagian dari code yang menjalankan tugas tertentu. 
+print() # adalah salah satu contoh function, untuk mencetak teks 
+
+# Seperti print, ada banyak function yang sudah tersedia 
+# di Python yang dapat membuat coding lebih mudah. 
+# Kita juga dapat membuat function Anda sendiri.
+
+# Sintaksis untuk mendefinisikan function adalah sebagai berikut:
+def greet():
+    print('hello')
+
+greet() #memanggil function greet yang sudah dibuat di atas
