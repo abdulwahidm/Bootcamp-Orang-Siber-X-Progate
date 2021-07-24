a2c47d3c525fd83ca24dbfb6051a8952673a67bf
abdulwahidm <wamuhabdul@gmail.com>
2021-07-24 13:13:54 +0700

    contoh penggunaan dictionary

--- Python-Study/Chapter-02/04-dictionary.py
+++ Python-Study/Chapter-02/04-dictionary.py
@@ -4,3 +4,10 @@ Perbedaannya adalah dictionary menggunakan kunci (key) daripada nomor index.
 Pada dictionary, sebuah kunci dipasangkan dengan sebuah nilai (value) untuk membentuk satu element. 
 Hal ini juga dikenal sebagai pasangan kunci-nilai (key-value).
 '''
+
+fruits = {'apel' : 'merah', 'pisang' : 'kuning', 'kelapa': 'hijau'}
+print(fruits) 
+#pada console mungkin urutan element dictionari akan berbeda dari yang didefinisikan
+#Ini dikarenakan, tidak seperti list, dictionary tidak memiliki index yang tetap
+
+print('Apel berwarna ' + fruits['apel']) #apel berwarna merah
