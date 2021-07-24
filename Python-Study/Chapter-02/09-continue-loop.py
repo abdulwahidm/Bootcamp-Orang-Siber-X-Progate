ba808d1d8959d3faa393701d23e3f7a7c316725b
abdulwahidm <wamuhabdul@gmail.com>
2021-07-25 02:43:59 +0700

    loop-continue

---
+++ Python-Study/Chapter-02/09-continue-loop.py
@@ -0,0 +1,10 @@
+# Tidak seperti statement break yang akan mengakhiri loop, 
+# statement continue melewatkan loop untuk iterasi tertentu. 
+# Statement continue dapat digunakan dengan cara yang sama pada loop for dan loop while.
+
+numbers = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
+for number in numbers:
+    # Lewatkan loop untuk angka yang dapat di bagi 3
+    if number % 3 == 0:
+        continue
+    print(number)
