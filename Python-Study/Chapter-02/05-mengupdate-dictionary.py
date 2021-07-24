cd32ae66d8d857c19e3664b0d989d0ca28813071
abdulwahidm <wamihabdul@gmail.com>
2021-07-24 14:08:00 +0700

    update value dictionary

---
+++ Python-Study/Chapter-02/05-mengupdate-dictionary.py
@@ -0,0 +1,14 @@
+#Seperti pada list, Anda dapat menambah atau memperbarui element pada dictionary.
+#Memperbarui nilai dari kunci yang sudah ada dapat dilakukan dengan menulis nama_dictionary[kunci] = nilai.
+
+
+fruits = {'apel': 1, 'pisang': 2, 'jeruk': 4}
+
+# Perbarui nilai dengan kunci 'pisang' ke 3
+fruits['pisang'] = 3
+
+# Tambahkan element ke fruits dengan kunci 'anggur' dan nilai 5
+fruits['anggur'] = 5
+
+# Cetak fruits
+print(fruits)
