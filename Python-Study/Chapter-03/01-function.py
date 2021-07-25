22f7d9db94cfbb8284d4dcb87303df4f4a0596f5
abdulwahidm <wamuhabdul@gmail.com>
2021-07-26 02:51:25 +0700

    endefinisikan function

---
+++ Python-Study/Chapter-03/01-function.py
@@ -0,0 +1,16 @@
+# Pertama-tama, mari kita lihat bagaimana function bekerja dalam Python. 
+# Function adalah bagian dari code yang menjalankan tugas tertentu. 
+# print adalah salah satu contoh function, yang memudahkan Anda 
+# untuk mencetak teks tanpa perlu untuk menulis banyak code.
+
+# Function tidak akan dijalankan hanya dengan mendefinisikannya, 
+# Anda perlu memanggil function untuk menjalankan code didalamnya. 
+# Untuk memanggil sebuah function, Anda cukup menulis function_name(). 
+# Perlu diingat bahwa function hanya dapat dipanggil setelah Anda mendefinisikannya.
+
+# Definisikan function print_hand 
+def print_hand():
+    print('Anda memilih: Batu')
+
+# Panggil function print_hand 
+print_hand()
