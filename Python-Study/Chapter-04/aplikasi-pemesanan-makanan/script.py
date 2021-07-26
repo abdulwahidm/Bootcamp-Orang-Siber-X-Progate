8f15e1c1b02d2ea1b66ae18caf2dbee4ee5d6953
abdulwahidm <wamuhabdul@gmail.com>
2021-07-26 16:12:57 +0700

    method init

--- Python-Study/Chapter-04/aplikasi-pemesanan-makanan/script.py
+++ Python-Study/Chapter-04/aplikasi-pemesanan-makanan/script.py
@@ -1,29 +1,24 @@
 class MenuItem:
-    def info(self):
-        # Cetak dengan format '____: $____'
-        print(self.name + ': $' + str(self.price))
+    def __init__(self):
+        # Tetapkan self.name ke 'Roti Lapis'
+        self.name = 'Roti Lapis'
+        
+        # Tetapkan self.price ke 5
+        self.price = 5
 
-    # Definiskan method get_total_price
+    def info(self):
+        return self.name + ': $' + str(self.price)
+
     def get_total_price(self, count):
         total_price = self.price * count
         return total_price
 
 
-
 menu_item1 = MenuItem()
-menu_item1.name = 'Roti Lapis'
-menu_item1.price = 5
+# Hapus dua baris di bawah
 
+print(menu_item1.info())
 
-menu_item2 = MenuItem()
-menu_item2.name = 'Kue Coklat'
-menu_item2.price = 4
-
-menu_item2.info()
-
-# Panggil method get_total_price 
 result = menu_item1.get_total_price(4)
-
-# Cetak 'Total harga adalah $____'
 print('Total harga adalah $' + str(result))
 
