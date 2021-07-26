5c7588c6a36300a959422883b7147b998b086e69
abdulwahidm <wamuhabdul@gmail.com>
2021-07-26 07:35:37 +0700

    menambahkan fungsi pemenang

--- Python-Study/Chapter-03/03-aplikasi-gunting-kertas-batu.py
+++ Python-Study/Chapter-03/03-aplikasi-gunting-kertas-batu.py
@@ -7,6 +7,20 @@ def print_hand(hand, name='Tamu'):
     hands = ['Batu', 'Kertas', 'Gunting']
     print(name + ' memilih: ' + hands[hand])
 
+# Definisikan function judge 
+def judge(player, computer):
+    # Tambahkan control flow berdasarkan perbandingan antara player dan computer
+    if player == computer:
+        return 'Seri'
+    elif player == 0 and computer == 1:
+        return 'Kalah'
+    elif player == 1 and computer == 2:
+        return 'Kalah'
+    elif player == 2 and computer == 0:
+        return 'Kalah'
+    else:
+        return 'Menang'
+
 print('Memulai permainan Batu Kertas Gunting!')
 player_name = input('Masukkan nama Anda: ')
 
@@ -14,12 +28,15 @@ print('Pilih tangan: (0: Batu, 1: Kertas, 2: Gunting)')
 player_hand = int(input('Masukkan nomor (0-2): '))
 
 if validate(player_hand):
-    # Tetapkan 1 ke variable computer_hand 
     computer_hand = 1
     
     print_hand(player_hand, player_name)
-    # Panggil function print_hand dengan computer_hand dan 'Komputer' sebagai argument
     print_hand(computer_hand, 'Komputer')
+
+    # Tetapkan nilai return dari judge ke variable result 
+    result = judge(player_hand, computer_hand)
+    # Cetak variable result 
+    print('Hasil: ' + result)
     
 else:
     print('Mohon masukkan nomor yang benar')
