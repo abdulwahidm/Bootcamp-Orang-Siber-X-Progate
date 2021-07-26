3ef1acb8761d4ce63dda53c8c816cd3da69d5231
abdulwahidm <wamuhabdul@gmail.com>
2021-07-26 16:06:19 +0700

    menambah method

--- Python-Study/Chapter-04/aplikasi-pemesanan-makanan/script.py
+++ Python-Study/Chapter-04/aplikasi-pemesanan-makanan/script.py
@@ -3,15 +3,27 @@ class MenuItem:
         # Cetak dengan format '____: $____'
         print(self.name + ': $' + str(self.price))
 
+    # Definiskan method get_total_price
+    def get_total_price(self, count):
+        total_price = self.price * count
+        return total_price
+
+
 
 menu_item1 = MenuItem()
 menu_item1.name = 'Roti Lapis'
 menu_item1.price = 5
 
-menu_item1.info()
 
 menu_item2 = MenuItem()
 menu_item2.name = 'Kue Coklat'
 menu_item2.price = 4
 
 menu_item2.info()
+
+# Panggil method get_total_price 
+result = menu_item1.get_total_price(4)
+
+# Cetak 'Total harga adalah $____'
+print('Total harga adalah $' + str(result))
+
